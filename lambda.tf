d9c64382a9bcfb57aa904ae2696442109717e3e9
Liorah <monteiroliorah@gmail.com>
2025-02-26 18:48:44 +0100

    updated

--- lambda.tf
+++ lambda.tf
@@ -2,13 +2,13 @@
 module "hello-world" {
   source                            = "terraform-aws-modules/lambda/aws"
   version                           = "7.20.0"
-  function_name                     = "hello-world"
+  function_name                     = "hello_world"
   description                       = "Testing if the API gateway works as a first trial to set up a lambda in an API gateway"
-  handler                           = "hello-world.lambda_handler"
+  handler                           = "hello_world.lambda_handler"
   runtime                           = "python3.11"
   architectures                     = ["arm64"]
   timeout                           = 120
-  source_path                       = "${path.module}/lambdas/hello-world/hello-world.py"
+  source_path                       = "${path.module}/lambdas/hello_world/"
   cloudwatch_logs_retention_in_days = 14
 
   #specifies that api_gateway can invoke the lambda
@@ -16,7 +16,7 @@ module "hello-world" {
     api_gateway = {
       statement_id  = "AllowAPIGatewayInvoke"
       action        = "lambda:InvokeFunction"
-      function_name = "hello-world"
+      function_name = "hello_world"
       principal     = "apigateway.amazonaws.com"
       source_arn    = "${aws_api_gateway_rest_api.ImsApi.execution_arn}/*/*" # Allow access from any method and path
     }
