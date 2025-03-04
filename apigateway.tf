edd49e0f04a05d99d2ed2a152a533458dabb393b
Liorah <monteiroliorah@gmail.com>
2025-03-05 00:58:19 +0100

    updated and added new lambda

--- apigateway.tf
+++ apigateway.tf
@@ -1,46 +1,45 @@
-# Source(s):
-# - https://medium.com/@m.farhatmaher/how-to-deploy-a-dash-app-on-aws-lambda-with-terraform-cbaa2f2bf9b1
-# - https://atsss.medium.com/small-lambda-function-with-python-and-terraform-f3100015f970 
+#source : https://medium.com/@m.farhatmaher/how-to-deploy-a-dash-app-on-aws-lambda-with-terraform-cbaa2f2bf9b1 ,
+#source : https://atsss.medium.com/small-lambda-function-with-python-and-terraform-f3100015f970 
 
-# Create a REST API Gateway
-resource "aws_api_gateway_rest_api" "ims_api" {
-  name        = "ims-rest-api"
-  description = "REST API Gateway for the Inventory Management System"
-
+resource "aws_api_gateway_rest_api" "ImsApi" {
+  name        = "Inventory-management-rest-api"
+  description = "Api Gateway Rest API for Inventory Management prototype"
+  body = templatefile("${path.module}/openapi.yaml",{
+    move_stock_item_arn = module.hello_world.lambda_function_invoke_arn
+  })
   endpoint_configuration {
     types = ["REGIONAL"]
   }
 }
 
-# Root resource to catch all requests so it would always be /hello/
+#root resource to catch all requests so it would always be /inventoryManagement/
 resource "aws_api_gateway_resource" "hello_resource" {
-  rest_api_id = aws_api_gateway_rest_api.ims_api.id
-  parent_id   = aws_api_gateway_rest_api.ims_api.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.ImsApi.id
+  parent_id   = aws_api_gateway_rest_api.ImsApi.root_resource_id
   path_part   = "hello"
 }
 
-# An example ethod for the root resource
+#method for the api resource which means the method will respond to ANY requests
 resource "aws_api_gateway_method" "hello_method" {
-  rest_api_id   = aws_api_gateway_rest_api.ims_api.id
+  rest_api_id   = aws_api_gateway_rest_api.ImsApi.id
   resource_id   = aws_api_gateway_resource.hello_resource.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
-# An example integration with a Lambda function
 resource "aws_api_gateway_integration" "hello_lambda_integration" {
-  rest_api_id = aws_api_gateway_rest_api.ims_api.id
+  # Integrate root proxy with the Lambda function
+  rest_api_id = aws_api_gateway_rest_api.ImsApi.id
   resource_id = aws_api_gateway_resource.hello_resource.id
   http_method = aws_api_gateway_method.hello_method.http_method
 
-  integration_http_method = "POST"      # AWS_PROXY requires a POST method
+  integration_http_method = "GET"
   type                    = "AWS_PROXY" # Direct integration with Lambda
   uri                     = module.hello_world.lambda_function_invoke_arn
 }
 
-# An example method success response (Status code 200)
 resource "aws_api_gateway_method_response" "hello_method_response" {
-  rest_api_id = aws_api_gateway_rest_api.ims_api.id
+  rest_api_id = aws_api_gateway_rest_api.ImsApi.id
   resource_id = aws_api_gateway_resource.hello_resource.id
   http_method = aws_api_gateway_method.hello_method.http_method
   status_code = "200" # Status code indicating a successful response
@@ -48,11 +47,12 @@ resource "aws_api_gateway_method_response" "hello_method_response" {
   response_parameters = {
     "method.response.header.Content-Type" = true # Indicates that the response will include this header
   }
+
+  depends_on = [module.hello_world]
 }
 
-# An example integration success response (Status code 200)
 resource "aws_api_gateway_integration_response" "hello_lambda_integration_response" {
-  rest_api_id = aws_api_gateway_rest_api.ims_api.id
+  rest_api_id = aws_api_gateway_rest_api.ImsApi.id
   resource_id = aws_api_gateway_resource.hello_resource.id
   http_method = aws_api_gateway_method.hello_method.http_method
   status_code = "200"
@@ -60,39 +60,27 @@ resource "aws_api_gateway_integration_response" "hello_lambda_integration_respon
   response_parameters = {
     "method.response.header.Content-Type" = "'application/json'" # Ensure the content type is set
   }
-
-  depends_on = [aws_api_gateway_integration.hello_lambda_integration]
+  depends_on = [module.hello_world]
 }
 
-# Deploy the API Gateway
+
 resource "aws_api_gateway_deployment" "ims_api_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.ImsApi.id
 
-  rest_api_id = aws_api_gateway_rest_api.ims_api.id
-
-  # Trigger the deployment when the resources change
-  # Note: Triggers are prefered over explicitly listed depends_on dependencies
   triggers = {
-    redeployment = sha1(jsonencode([
-      aws_api_gateway_resource.hello_resource.id,
-      aws_api_gateway_method.hello_method.id,
-      aws_api_gateway_integration.hello_lambda_integration.id,
-    ]))
+    redeployment = sha1(jsonencode(aws_api_gateway_resource.hello_resource))
   }
+
+  depends_on = [aws_api_gateway_integration.hello_lambda_integration]
 
   lifecycle {
     create_before_destroy = true
   }
-
 }
 
-# Create a stage for the deployment
 resource "aws_api_gateway_stage" "ims_api_stage_deployment" {
   deployment_id = aws_api_gateway_deployment.ims_api_deployment.id
-  rest_api_id   = aws_api_gateway_rest_api.ims_api.id
+  rest_api_id   = aws_api_gateway_rest_api.ImsApi.id
 
   stage_name = "dev"
-
-  lifecycle {
-    ignore_changes = [deployment_id]
-  }
 }
--- lambda.tf
+++ lambda.tf
@@ -19,3 +19,24 @@ module "hello_world" {
     }
   }
 }
+
+module "move_store_item" {
+  source        = "terraform-aws-modules/lambda/aws"
+  version       = "7.20.0"
+  function_name = "move_store_item"
+  description   = "Move item from one store to another "
+  handler       = "move_store_item.lambda_handler"
+  runtime       = "python3.13"
+  architectures = ["arm64"]
+  timeout       = 120
+  source_path   = "${path.module}/lambdas/move_store_item/"
+  publish       = true
+
+  # Allow the API Gateway to invoke the Lambda functions
+  allowed_triggers = {
+    APIGatewayAny = {
+      service    = "apigateway"
+      source_arn = "${aws_api_gateway_rest_api.ims_api.execution_arn}/*/*" # Allow access from any method and path
+    }
+  }
+}
