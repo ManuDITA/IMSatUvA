6c27141e9972d4557d15fa81864f5b40b7557e26
Liorah <monteiroliorah@gmail.com>
2025-03-05 17:06:58 +0100

    updated the persmission

--- iam.tf
+++ iam.tf
@@ -23,3 +23,17 @@ resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
   depends_on = [aws_iam_role.lambda_exec_role]
 }
+
+resource "aws_lambda_permission" "allow_api_gateway" {
+  for_each = {
+    for fn in [module.move_store_item] : fn.function_name => fn
+  }
+
+  statement_id  = "${each.key}-allow-api-gateway"
+  action        = "lambda:InvokeFunction"
+  function_name = each.value.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # This ARN allows API Gateway to invoke the Lambda function
+  source_arn    = "${aws_api_gateway_rest_api.ims_api.execution_arn}/*/*"
+}
