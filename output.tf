eef4df943c37f0352f1a5aa91053e959fe4e40d7
Liorah <monteiroliorah@gmail.com>
2025-03-05 15:58:11 +0100

    hoping this works

--- output.tf
+++ output.tf
@@ -2,3 +2,6 @@
 output "lambda_function_hello_world_invoke_arn" { 
     value = module.hello_world.lambda_function_invoke_arn
 }
+output "lambda_function_move_store_item_invoke_arn" { 
+    value = module.move_store_item.lambda_function_invoke_arn
+}
