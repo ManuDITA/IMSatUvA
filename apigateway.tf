b9401b47756307d8983b3b94b946bbd60c8a844d
Liorah <monteiroliorah@gmail.com>
2025-03-05 12:26:33 +0100

    temporarily removed triggers

--- apigateway.tf
+++ apigateway.tf
@@ -36,7 +36,7 @@ resource "aws_api_gateway_integration" "hello_lambda_integration" {
   resource_id = aws_api_gateway_resource.hello_resource.id
   http_method = aws_api_gateway_method.hello_method.http_method
 
-  integration_http_method = "GET"      # AWS_PROXY requires a POST method
+  integration_http_method = "POST"      # AWS_PROXY requires a POST method
   type                    = "AWS_PROXY" # Direct integration with Lambda
   uri                     = module.hello_world.lambda_function_invoke_arn
 }
@@ -72,15 +72,8 @@ resource "aws_api_gateway_deployment" "ims_api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.ims_api.id
 
   # Trigger the deployment when the resources change
-  # Note: Triggers are prefered over explicitly listed depends_on dependencies
-  triggers = {
-    redeployment = sha1(jsonencode([
-      aws_api_gateway_resource.hello_resource.id,
-      aws_api_gateway_method.hello_method.id,
-      aws_api_gateway_integration.hello_lambda_integration.id,
-      aws_api_gateway_rest_api.ims_api.body
-    ]))
-  }
+  # Note: Triggers are preferred over explicitly listed depends_on dependencies
+
 
   lifecycle {
     create_before_destroy = true
