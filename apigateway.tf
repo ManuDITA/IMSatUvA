b9b3102a219f8309af465962bf717fac64b465a2
Liorah <monteiroliorah@gmail.com>
2025-03-05 16:26:26 +0100

    removed helloworld stuff

--- apigateway.tf
+++ apigateway.tf
@@ -15,58 +15,6 @@ resource "aws_api_gateway_rest_api" "ims_api" {
   }
 }
 
-# Root resource to catch all requests so it would always be /hello/
-resource "aws_api_gateway_resource" "hello_resource" {
-  rest_api_id = aws_api_gateway_rest_api.ims_api.id
-  parent_id   = aws_api_gateway_rest_api.ims_api.root_resource_id
-  path_part   = "hello"
-}
-
-# An example method for the root resource
-resource "aws_api_gateway_method" "hello_method" {
-  rest_api_id   = aws_api_gateway_rest_api.ims_api.id
-  resource_id   = aws_api_gateway_resource.hello_resource.id
-  http_method   = "GET"
-  authorization = "NONE"
-}
-
-# An example integration with a Lambda function
-resource "aws_api_gateway_integration" "hello_lambda_integration" {
-  rest_api_id = aws_api_gateway_rest_api.ims_api.id
-  resource_id = aws_api_gateway_resource.hello_resource.id
-  http_method = aws_api_gateway_method.hello_method.http_method
-
-  integration_http_method = "POST"      # AWS_PROXY requires a POST method
-  type                    = "AWS_PROXY" # Direct integration with Lambda
-  uri                     = module.hello_world.lambda_function_invoke_arn
-}
-
-# An example method success response (Status code 200)
-resource "aws_api_gateway_method_response" "hello_method_response" {
-  rest_api_id = aws_api_gateway_rest_api.ims_api.id
-  resource_id = aws_api_gateway_resource.hello_resource.id
-  http_method = aws_api_gateway_method.hello_method.http_method
-  status_code = "200" # Status code indicating a successful response
-
-  response_parameters = {
-    "method.response.header.Content-Type" = true # Indicates that the response will include this header
-  }
-}
-
-# An example integration success response (Status code 200)
-resource "aws_api_gateway_integration_response" "hello_lambda_integration_response" {
-  rest_api_id = aws_api_gateway_rest_api.ims_api.id
-  resource_id = aws_api_gateway_resource.hello_resource.id
-  http_method = aws_api_gateway_method.hello_method.http_method
-  status_code = "200"
-
-  response_parameters = {
-    "method.response.header.Content-Type" = "'application/json'" # Ensure the content type is set
-  }
-
-  depends_on = [aws_api_gateway_integration.hello_lambda_integration]
-}
-
 # Deploy the API Gateway
 resource "aws_api_gateway_deployment" "ims_api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.ims_api.id
