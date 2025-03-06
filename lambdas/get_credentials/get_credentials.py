e2a1913df6b818c1cfd5bb295e3e7a3454a7437b
martin-r-georgiev <martin.r.georgiev@hotmail.com>
2025-03-06 02:15:01 +0100

    Updated identity client

--- lambdas/get_credentials/get_credentials.py
+++ lambdas/get_credentials/get_credentials.py
@@ -4,12 +4,14 @@ import os
 
 def lambda_handler(event, context):
     id_token = event["headers"]["Authorization"]
-    client = boto3.client("cognito-idp")
     aws_region = os.getenv("TF_AWS_REGION")
     identity_pool_id = os.getenv("IDENTITY_POOL_ID")
 
+    # Initialize the Cognito Identity client
+    identity_client = boto3.client("cognito-identity")
+
     # Get the identity id
-    identity_response = client.get_id(
+    identity_response = identity_client.get_id(
         IdentityPoolId = identity_pool_id,
         Logins = {
             f"cognito-idp.{aws_region}.amazonaws.com/{identity_pool_id}": id_token
@@ -18,7 +20,7 @@ def lambda_handler(event, context):
     identity_id = identity_response["IdentityId"]
 
     # Get the user's credentials from the identity pool
-    credentials_response = client.get_credentials_for_identity(
+    credentials_response = identity_client.get_credentials_for_identity(
         IdentityId = identity_id,
         Logins = {
             f"cognito-idp.{aws_region}.amazonaws.com/{identity_pool_id}": id_token
