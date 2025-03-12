c5c17232700d13a8f55d2910b01012a638a6e0fd
Emanuele Simeone <emanuelesimeone0@gmail.com>
2025-03-12 22:13:38 +0100

    added remove store item definition

--- lambdas/remove_store_item/remove_store_item.py
+++ lambdas/remove_store_item/remove_store_item.py
@@ -3,27 +3,27 @@ import boto3
 from botocore.exceptions import ClientError
 
 dynamodb = boto3.resource('dynamodb')
-store_items_table = dynamodb.Table('InventoryItem')
+store_table = dynamodb.Table('store')
 
 def lambda_handler(event, context):
     # Extract path parameters from the event
     store_id = event['pathParameters']['storeId']
     stock_id = event['pathParameters']['stockId']
     
-    # Extract body parameters from the event
-    body = json.loads(event['body'])
-    quantity = body['quantity']
-    to_store = body['toStore']
-
-    try:
-        {
-        }
+    deleteAttributes = store_table.delete_item(store_id, ReturnValues='ALL_OLD')
+    if len(deleteAttributes.keys() == 0):
+        return 400, 'Store not found'
     
-    except ClientError as e:
-        return {
-            'statusCode': 400,
-            'body': json.dumps({
-                'message': 'Error removing item.',
-                'error': str(e)
-            })
-        }
+    # Extract body parameters from the event
+    body = json.dumps(deleteAttributes)
+    
+    res = {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": body
+    }
+
+    return res
+
