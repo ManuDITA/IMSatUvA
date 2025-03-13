03139be6e84ad071c681feb370358c769ab7e675
martin-r-georgiev <martin.r.georgiev@hotmail.com>
2025-03-13 18:09:01 +0100

    Improved http util function

--- lambdas/modules/http_utils.py
+++ lambdas/modules/http_utils.py
@@ -1,10 +1,16 @@
 import json
 
-def generate_response(status_code, message): 
+def generate_response(status_code, message):
+    body = {}
+    if type(message) == dict or type(message) == list:
+        body = message
+    else:
+        body = {"message": message}
+
     return {
         "statusCode": status_code,
         "headers": {
             "Content-Type": "application/json"
         },
-        "body": json.dumps({"message": message})
+        "body": json.dumps(body)
     }
