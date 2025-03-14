c59abb457e4754beb83a411c8714cef2aad6a4f0
Emanuele Simeone <emanuelesimeone0@gmail.com>
2025-03-14 14:18:26 +0100

    fixed method, testing required

--- lambdas/remove_store_item/remove_store_item.py
+++ lambdas/remove_store_item/remove_store_item.py
@@ -1,29 +1,25 @@
 import json
 import boto3
-from botocore.exceptions import ClientError
+import modules.http_utils as http_utils
 
-dynamodb = boto3.resource('dynamodb')
-store_table = dynamodb.Table('store')
+dynamodb = boto3.resource("dynamodb")
+store_table = dynamodb.Table("store")
+
 
 def lambda_handler(event, context):
-    # Extract path parameters from the event
     store_id = event['pathParameters']['storeId']
     stock_id = event['pathParameters']['stockId']
-    
-    deleteAttributes = store_table.delete_item(store_id, ReturnValues='ALL_OLD')
-    if len(deleteAttributes.keys() == 0):
-        return 400, 'Store not found'
-    
-    # Extract body parameters from the event
-    body = json.dumps(deleteAttributes)
-    
-    res = {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": body
-    }
 
-    return res
+    try:
+        response = store_table.delete_item(
+            Key={'storeId': store_id, 'stockId': stock_id},
+            ReturnValues='ALL_OLD'
+        )
+    except KeyError:
+        return http_utils.generate_response(404, 'Resource not found')
 
+    # If the item was not found, the response will not contain the 'Attributes' key
+    if 'Attributes' not in response:
+        return http_utils.generate_response(404, 'Resource not found')
+
+    return http_utils.generate_response(200, response['Attributes'])
