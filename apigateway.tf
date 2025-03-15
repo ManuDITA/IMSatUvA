e501cd80fb8498947bbb580d50e3426e5bb906a4
Emanuele Simeone <emanuelesimeone0@gmail.com>
2025-03-15 14:40:27 +0100

    added patch method logic and tf api definition for patch method

--- apigateway.tf
+++ apigateway.tf
@@ -24,6 +24,7 @@ resource "aws_api_gateway_rest_api" "ims_api" {
     auth_test_admin_arn   = module.auth_test_admin.lambda_function_invoke_arn
     get_credentials_arn   = module.get_credentials.lambda_function_invoke_arn
     add_store_item_arn    = module.add_store_item.lambda_function_invoke_arn
+    update_store_item_arn = module.update_store_item.lambda_function_invoke_arn
     delete_store_item_arn = module.delete_store_item.lambda_function_invoke_arn
     cognito_user_pool_arn = aws_cognito_user_pool.ims_user_pool.arn
   })))
