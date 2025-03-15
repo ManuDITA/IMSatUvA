612e2aab8c133de92cc003dbcdecf027479b7f36
martin-r-georgiev <martin.r.georgiev@hotmail.com>
2025-03-15 14:44:29 +0100

    Added safeguard to stop admins from demoting themselves

--- lambdas/demote_user/demote_user.py
+++ lambdas/demote_user/demote_user.py
@@ -11,6 +11,19 @@ def lambda_handler(event, context):
     body = json.loads(event['body'])
     group_name = body['groupName']
 
+     # Extract the caller's user ID from cognitoAuthenticationProvider
+    auth_provider = event['requestContext']['identity']['cognitoAuthenticationProvider']
+    caller_user_id = auth_provider.split(':')[-1]  # Extract the user ID (last part of the string)
+
+    response = cognito_client.list_users(
+        UserPoolId=USER_POOL_ID,
+        Filter=f'sub = "{caller_user_id}"'
+    )
+
+    # Admins should not be allowed to remove themselves from the Admin group
+    if group_name == 'Admins' and response['Users'] and response['Users'][0]['Username'] == username:
+        return http_utils.generate_response(403, "You cannot remove yourself from the Admin group")
+
     try:
         # Remove the user from the specified group
         cognito_client.admin_remove_user_from_group(
