7e6f6d8b1c26d5a295215a874859058566601435
Emanuele Simeone <emanuelesimeone0@gmail.com>
2025-03-16 15:30:15 +0100

    removed duplicate move_store_item

--- lambda.tf
+++ lambda.tf
@@ -395,28 +395,6 @@ module "delete_store" {
   }
 }
 
-module "move_store_item" {
-  source        = "terraform-aws-modules/lambda/aws"
-  version       = "7.20.0"
-  function_name = "move_store_item-${terraform.workspace}"
-  description   = "Move item from one store to another "
-  handler       = "move_store_item.lambda_handler"
-  runtime       = "python3.13"
-  architectures = ["arm64"]
-  timeout       = 120
-  create_role   = false
-  lambda_role   = aws_iam_role.lambda_exec_role.arn
-  source_path   = "${path.module}/lambdas/move_store_item/"
-  publish       = true
-
-  # Allow the API Gateway to invoke the Lambda functions
-  allowed_triggers = {
-    APIGatewayAny = {
-      service    = "apigateway"
-      source_arn = "${aws_api_gateway_rest_api.ims_api.execution_arn}/*/*" # Allow access from any method and path
-    }
-  }
-}
 module "add_store_item" {
   source        = "terraform-aws-modules/lambda/aws"
   version       = "7.20.0"
