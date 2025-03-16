b6f946acb7416dfcb49c35fa9dd2e44ee25a68a2
Liorah <monteiroliorah@gmail.com>
2025-03-17 00:23:10 +0100

    remve works

--- apigateway.tf
+++ apigateway.tf
@@ -10,24 +10,25 @@ resource "aws_api_gateway_rest_api" "ims_api" {
   description = "REST API Gateway for the Inventory Management System"
 
   body = jsonencode(yamldecode(templatefile("${path.module}/openapi.yaml", {
-    get_all_items_arn     = module.get_all_items.lambda_function_invoke_arn
-    register_item_arn     = module.register_item.lambda_function_invoke_arn
-    get_item_arn          = module.get_item.lambda_function_invoke_arn
-    delete_item_arn       = module.delete_item.lambda_function_invoke_arn
-    move_stock_item_arn   = module.move_store_item.lambda_function_invoke_arn
-    get_all_stores_arn    = module.get_all_stores.lambda_function_invoke_arn
-    add_store_arn         = module.add_store.lambda_function_invoke_arn
-    get_store_arn         = module.get_store.lambda_function_invoke_arn
-    delete_store_arn      = module.delete_store.lambda_function_invoke_arn
-    move_stock_item_arn   = module.move_store_item.lambda_function_invoke_arn
-    auth_test_user_arn    = module.auth_test_user.lambda_function_invoke_arn
-    auth_test_admin_arn   = module.auth_test_admin.lambda_function_invoke_arn
-    get_credentials_arn   = module.get_credentials.lambda_function_invoke_arn
-    promote_user_arn      = module.promote_user.lambda_function_invoke_arn
-    demote_user_arn       = module.demote_user.lambda_function_invoke_arn
-    cognito_user_pool_arn = aws_cognito_user_pool.ims_user_pool.arn
-    get_cart_arn          = module.get_cart.lambda_function_invoke_arn
-    add_item_to_cart_arn  = module.add_item_to_cart.lambda_function_invoke_arn 
+    get_all_items_arn         = module.get_all_items.lambda_function_invoke_arn
+    register_item_arn         = module.register_item.lambda_function_invoke_arn
+    get_item_arn              = module.get_item.lambda_function_invoke_arn
+    delete_item_arn           = module.delete_item.lambda_function_invoke_arn
+    move_stock_item_arn       = module.move_store_item.lambda_function_invoke_arn
+    get_all_stores_arn        = module.get_all_stores.lambda_function_invoke_arn
+    add_store_arn             = module.add_store.lambda_function_invoke_arn
+    get_store_arn             = module.get_store.lambda_function_invoke_arn
+    delete_store_arn          = module.delete_store.lambda_function_invoke_arn
+    move_stock_item_arn       = module.move_store_item.lambda_function_invoke_arn
+    auth_test_user_arn        = module.auth_test_user.lambda_function_invoke_arn
+    auth_test_admin_arn       = module.auth_test_admin.lambda_function_invoke_arn
+    get_credentials_arn       = module.get_credentials.lambda_function_invoke_arn
+    promote_user_arn          = module.promote_user.lambda_function_invoke_arn
+    demote_user_arn           = module.demote_user.lambda_function_invoke_arn
+    cognito_user_pool_arn     = aws_cognito_user_pool.ims_user_pool.arn
+    get_cart_arn              = module.get_cart.lambda_function_invoke_arn
+    add_item_to_cart_arn      = module.add_item_to_cart.lambda_function_invoke_arn 
+    remove_item_from_cart_arn = module.remove_item_from_cart.lambda_function_invoke_arn
   })))
 
   endpoint_configuration {
