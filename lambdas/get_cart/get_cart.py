545f82ce06f4e604c50c87fbe616d7e8ff0db907
Liorah <monteiroliorah@gmail.com>
2025-03-16 21:50:38 +0100

    updated to get cart

--- lambdas/get_cart/get_cart.py
+++ lambdas/get_cart/get_cart.py
@@ -4,16 +4,29 @@ import boto3
 import modules.http_utils as http_utils
 
 dynamodb = boto3.resource("dynamodb")
-table = dynamodb.Table('cart')
+cart_table = dynamodb.Table('cart')
 
 def lambda_handler(event, context):
+     # Extract the caller's user ID from cognitoAuthenticationProvider
+    auth_provider = event['requestContext']['identity']['cognitoAuthenticationProvider']
+    userId = auth_provider.split(':')[-1]  # Extract the user ID (last part of the string)
     
-    store = {}
+    if not userId:
+        return http_utils.generate_response(401, 'Not authorized')
+    
     try:
-        store = table.get_item(Key={'id': store_id})['Item']
-    except KeyError:
-        return http_utils.generate_response(404, 'Resource not found')
+        store_id = event['pathParameters']['storeId']
+        
+        user_cart = cart_table.get_item(Key={'userId': userId, 'storeId': store_id})
+        if not user_cart :
+            return http_utils.generate_response(400, f"Cart doesn't exist for this user in this store, call add item to cart to create new")
+        
+        return http_utils.generate_response(200, user_cart)
+    except KeyError as e:
+        return http_utils.generate_response(400, f"Missing required parameter: {str(e)}")
+    except Exception as e:
+        return http_utils.generate_response(500, f"Internal server error: {str(e)}")
+    
 
-    return http_utils.generate_response(200, store)
 
 
