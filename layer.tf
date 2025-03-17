135fec6638957ef70c34e34a3aadcedb82a1754b
martin-r-georgiev <martin.r.georgiev@hotmail.com>
2025-03-17 20:25:10 +0100

    Fixes to lambda zip archive directory structure

--- layer.tf
+++ layer.tf
@@ -1,5 +1,5 @@
 resource "aws_lambda_layer_version" "lambda_layer" {
-  filename            = "${path.module}/layer/python/lambda_layer.zip"
+  filename            = "${path.module}/layers/lambda_layer.zip"
   layer_name          = "lambda-layer"
   compatible_runtimes = ["python3.13"]
 }
